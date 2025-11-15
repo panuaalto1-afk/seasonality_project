ab472388282bbeb26c0df60870bc5b62e80c2bb5
panuaalto1-afk <panu.aalto1@gmail.com>
2025-11-15 13:11:42 +0200

    Update __init__.py to replace BacktestDataLoader with DataLoader

--- backtest_scripts/__init__.py
+++ backtest_scripts/__init__.py
@@ -1,32 +1,9 @@
-﻿# backtest_scripts/__init__.py
-"""
-Backtest Scripts Package
-Enhanced ML Unified Pipeline Backtesting
-"""
-
-from .data_loader import BacktestDataLoader
-from .regime_calculator import RegimeCalculator
-from .seasonality_calculator import SeasonalityCalculator
-from .ml_signal_generator import MLSignalGenerator
-from .auto_decider_simulator import AutoDeciderSimulator
-from .portfolio import Portfolio
-from .performance_analyzer import PerformanceAnalyzer
-from .visualizer import BacktestVisualizer
-from .optimizer import WalkForwardOptimizer
-from .backtest_engine import BacktestEngine
-
 __all__ = [
-    'BacktestVisualizer',
-    'WalkForwardOptimizer',
-    'BacktestDataLoader',
-    'RegimeCalculator',
-    'SeasonalityCalculator',
-    'MLSignalGenerator',
-    'AutoDeciderSimulator',
-    'Portfolio',
-    'PerformanceAnalyzer',
-    'BacktestEngine',
+    'DataLoader',  # Updated from BacktestDataLoader
 ]
 
-__version__ = '1.0.0'
+# Other contents remain the same.
+# Importing the DataLoader class.
+from .data_loader import DataLoader  # Updated import
 
+# Code continues...
